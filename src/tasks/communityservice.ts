d551ec0a4b927474c61e1fc83affc8ced068e7d2
AJ Coppa <aj.coppa@gmail.com>
2023-03-26 11:20:57 -0400

    Remove leftover goorbo references

--- src/tasks/communityservice.ts
+++ src/tasks/communityservice.ts
@@ -254,10 +254,10 @@ export function CSQuests(): Quest[] {
           completed: () => stooperDrunk(),
           do: (): void => {
             const targetAdvs = 100 - numericModifier("adventures");
-            print("goorbo completed, but did not overdrink.", "red");
+            print("allday completed, but did not overdrink.", "red");
             if (targetAdvs < myAdventures() && targetAdvs > 0)
               print(
-                `Rerun with fewer than ${targetAdvs} adventures for goorbo to handle your diet`,
+                `Rerun with fewer than ${targetAdvs} adventures for allday to handle your diet`,
                 "red"
               );
             else print("Something went wrong.", "red");
